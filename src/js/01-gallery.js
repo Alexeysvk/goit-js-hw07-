ac49bf5ca425a1e8674ab123c05608338da7e044
Alexeysvk <83663482+Alexeysvk@users.noreply.github.com>
2022-05-04 21:04:28 +0400

    gg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css"
+import templateFunction from "../templates/item-markup.hbs";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryRefs = document.querySelector('.gallery');
+
+(function createMarkup() {
+  const itemMarkup = galleryItems.map(templateFunction).join('');
+  galleryRefs.insertAdjacentHTML('beforeend', itemMarkup);
+})();
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
