857b9ff881a1d41138629592f4179a95ec100d3f
Alexeysvk <83663482+Alexeysvk@users.noreply.github.com>
2022-05-06 21:32:49 +0300

    gggg

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash/throttle';
 
+const ref = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const email = ref.form.elements.email;
+const message = ref.form.elements.message;
+
+ref.form.addEventListener('input', throttle(onFormInput, 500));
+ref.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+populateData();
+
+function populateData() {
+  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!parsedData) return;
+  email.value = parsedData.email;
+  message.value = parsedData.message;
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (!email.value || !message.value) {
+    return alert('All feels must be fell out!');
+  }
+  console.log({ email: email.value, message: message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
